fd2babee3283d018342d1fe100293c43442a05a8
parpa <parpa@DESKTOP-1JUTT8T>
2021-09-06 10:41:17 +0300

    Navbar changed + responsiveness done better

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
 function onFormSubmit() {
     var formData = readFormData();
     insertNewRecord(formData);
